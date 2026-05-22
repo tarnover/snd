4ddcab220551451afc189f3b310c3298ede65b04
Jascha <jaschadub@gmail.com>
2026-05-22 16:13:04 -0700

    feat(snd): drop unused assets/platform imports from header.js

--- app/ui/header.js
+++ app/ui/header.js
@@ -1,8 +1,6 @@
 const html = require('choo/html');
 const Component = require('choo/component');
 const Account = require('./account');
-const assets = require('../../common/assets');
-const { platform } = require('../utils');
 
 class Header extends Component {
   constructor(name, state, emit) {
