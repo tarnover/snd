b49dfedb6159e7575466e4580bcffb49a76d0531
Jascha <jaschadub@gmail.com>
2026-05-22 16:23:33 -0700

    feat(snd): restyle download-complete screen

--- app/ui/downloadCompleted.js
+++ app/ui/downloadCompleted.js
@@ -6,16 +6,16 @@ module.exports = function(state) {
   return html`
     <div
       id="download-complete"
-      class="flex flex-col items-center justify-center h-full w-full bg-white p-2 dark:bg-grey-90"
+      class="flex flex-col items-center justify-center w-full p-2"
     >
-      <h1 class="text-center text-3xl font-bold my-2">
+      <h1 class="snd-display text-center my-2">
         ${state.translate('downloadFinish')}
       </h1>
       <svg class="my-8 h-48 text-primary">
         <use xlink:href="${assets.get('completed.svg')}#Page-1" />
       </svg>
       <p
-        class="text-grey-80 leading-normal dark:text-grey-40 ${state.user
+        class="snd-body snd-text-mute leading-normal ${state.user
           .loggedIn
           ? 'hidden'
           : ''}"
@@ -23,7 +23,7 @@ module.exports = function(state) {
         ${state.translate('trySendDescription')}
       </p>
       <p class="my-5">
-        <a href="/" class="btn rounded-lg flex items-center mt-4" role="button"
+        <a href="/" class="snd-btn snd-btn--primary flex items-center mt-4" role="button"
           >${state.translate(btnText)}</a
         >
       </p>
