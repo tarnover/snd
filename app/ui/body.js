2176f0dc08bcbe424149e519dbf1e1f449375bd0
Jascha <jaschadub@gmail.com>
2026-05-22 16:10:38 -0700

    feat(snd): wire status bar into body shell

--- app/ui/body.js
+++ app/ui/body.js
@@ -1,19 +1,19 @@
 const html = require('choo/html');
 const Header = require('./header');
 const Footer = require('./footer');
+const statusBar = require('./statusBar');
 
 module.exports = function body(main) {
   return function(state, emit) {
     const b = html`
-      <body
-        class="flex flex-col items-center font-sans md:h-screen md:bg-grey-10 dark:bg-black"
-      >
-        ${state.cache(Header, 'header').render()} ${main(state, emit)}
+      <body class="snd-body-shell flex flex-col">
+        ${state.cache(Header, 'header').render()}
+        ${main(state, emit)}
         ${state.cache(Footer, 'footer').render()}
+        ${statusBar(state)}
       </body>
     `;
     if (state.layout) {
-      // server side only
       return state.layout(state, b);
     }
     return b;
