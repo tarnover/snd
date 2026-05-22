8207f3936f3243c906581d8a5a1c765ea97eaf3b
Jascha <jaschadub@gmail.com>
2026-05-22 16:15:39 -0700

    feat(snd): restyle intro section to Vault eyebrow + display

--- app/ui/intro.js
+++ app/ui/intro.js
@@ -3,11 +3,11 @@ const raw = require('choo/html/raw');
 const assets = require('../../common/assets');
 
 module.exports = function intro(state) {
+  const t = key => state.translate(key);
+
   const notice = state.WEB_UI.MAIN_NOTICE_HTML
     ? html`
-        <p
-          class="w-full mt-2 p-2 border-default dark:border-grey-70 rounded-default text-orange-60 bg-yellow-40 text-center leading-normal"
-        >
+        <p class="snd-notice w-full text-center mt-2">
           ${raw(state.WEB_UI.MAIN_NOTICE_HTML)}
         </p>
       `
@@ -16,7 +16,7 @@ module.exports = function intro(state) {
   const sponsor = state.WEB_UI.SHOW_THUNDERBIRD_SPONSOR
     ? html`
         <a
-          class="w-full mt-5 mb-2 p-2 border-default dark:border-grey-70 rounded-default text-orange-60 bg-yellow-40 text-center leading-normal"
+          class="snd-notice w-full text-center mt-4"
           href="https://www.thunderbird.net/"
         >
           <svg
@@ -37,19 +37,12 @@ module.exports = function intro(state) {
     : '';
 
   return html`
-    <send-intro
-      class="flex flex-col items-center justify-center bg-white px-6 md:py-0 py-6 mb-0 h-full w-full dark:bg-grey-90"
-    >
+    <section class="snd-intro">
+      <p class="snd-tag">NEW TRANSFER</p>
+      <h1 class="snd-display-xl snd-intro-title">${t('introTitle')}</h1>
+      <p class="snd-body snd-intro-body">${t('introDescription')}</p>
       ${notice}
-      <div class="mt-12 flex flex-col h-full">
-        <h1 class="text-3xl font-bold md:pb-2">
-          ${state.translate('introTitle')}
-        </h1>
-        <p class="max-w-sm leading-loose mt-6 md:mt-2 md:pr-14">
-          ${state.translate('introDescription')}
-        </p>
-      </div>
       ${sponsor}
-    </send-intro>
+    </section>
   `;
 };
